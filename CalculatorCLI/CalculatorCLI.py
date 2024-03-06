59810f5e072eb622cda329a0b9269a5968d5f1e3
Mark Crowe <66536097+marcocrowe@users.noreply.github.com>
2024-03-06 11:31:38 +0000

    format, cleanup 2/3

--- CalculatorCLI/CalculatorCLI.py
+++ CalculatorCLI/CalculatorCLI.py
@@ -1,10 +1,13 @@
-# Copyright (c) 2021 Mark Crowe <https://github.com/markcrowe-com>. All rights reserved.
+"""
+Copyright © 2024 Mark Crowe <https://github.com/marcocrowe>. All rights reserved.
+Hello World
+"""
 
 
 class CalculatorCLI:
     """A Command Line Interface (CLI) Calculator"""
 
-    def printMenu(self):
+    def print_menu(self) -> None:
         """Prints the menu"""
         print("1. Add")
         print("2. Subtract")
@@ -14,67 +17,93 @@ class CalculatorCLI:
 
     def divide(self):
         """Divides a number from another number"""
-        dividend = self.inputFloat("Enter Dividend number: ")
-        divisor = self.inputFloat("Enter divisor number: ")
+        dividend: float = self.get_float_input("Enter Dividend number: ")
+        divisor: float = self.get_float_input("Enter divisor number: ")
         if divisor == 0:
             print("Division by 0 is not defined.")
         else:
             print("{} / {} = {}".format(dividend, divisor, dividend / divisor))
 
-    def inputFloat(self, text="Enter Number"):
-        """Reads a float by reading and coverting a string from standard Input."""
-        """If an invalid float format is entered the function will continue to try to get a valid float from standard Input"""
-        """If the user hits EOF (*nix: Ctrl-D, Windows: Ctrl-Z+Return), raise SystemExit. On *nix systems, readline is used if available."""
+    @staticmethod
+    def get_float_input(text: str = "Enter Number: ") -> float:
+        """Reads a float by reading and converting a string from standard Input. If an invalid
+        float format is entered the function will continue to try to get a valid float from
+        standard Input If the user hits EOF (*nix: Ctrl-D , Windows: Ctrl-Z+Return), raise
+        SystemExit. On *nix systems, readline is used if available.
+
+        Args:
+            text (str, optional): The prompt to display. Defaults to "Enter Number: ".
+
+        Raises:
+            SystemExit: If the user hits EOF (*nix: Ctrl-D , Windows: Ctrl-Z+Return)
+
+        Returns:
+            float: The float entered by the user
+        """
         while True:
             try:
                 return float(input(text))
             except ValueError:
                 print("Not a Float")
-            except (KeyboardInterrupt, EOFError):
-                raise SystemExit
+            except (KeyboardInterrupt, EOFError) as exception:
+                raise SystemExit from exception
 
-    def inputInt(self, text="Enter Integer"):
-        """Reads a int by reading and coverting a string from standard Input."""
-        """If an invalid int format is entered the function will continue to try to get a valid int from standard Input"""
-        """If the user hits EOF (*nix: Ctrl-D, Windows: Ctrl-Z+Return), raise SystemExit. On *nix systems, readline is used if available."""
+    @staticmethod
+    def get_int_input(text: str = "Enter Number: ") -> int:
+        """Reads a int by reading and converting a string from standard Input. If an invalid
+        int format is entered the function will continue to try to get a valid int from
+        standard Input If the user hits EOF (*nix: Ctrl-D , Windows: Ctrl-Z+Return), raise
+        SystemExit. On *nix systems, readline is used if available.
+
+        Args:
+            text (str, optional): The prompt to display. Defaults to "Enter Number: ".
+
+        Raises:
+            SystemExit: If the user hits EOF (*nix: Ctrl-D , Windows: Ctrl-Z+Return)
+
+        Returns:
+            int: The float entered by the user
+        """
         while True:
             try:
                 return int(input(text))
             except ValueError:
                 print("Not a Integer")
-            except (KeyboardInterrupt, EOFError):
-                raise SystemExit
+            except (KeyboardInterrupt, EOFError) as exception:
+                raise SystemExit from exception
 
     def multiply(self):
         """Multiplies a series of numbers"""
-        count = self.inputInt("How many number do you want to multiply: ")
+        count = self.get_int_input("How many number do you want to multiply: ")
         product = 1.0
         for number in range(count):
-            product *= self.inputFloat(
+            product *= self.get_float_input(
                 "Enter multiplier number {} of {}: ".format(number + 1, count)
             )
         print("Product = {}".format(product))
 
     def subtract(self):
         """Subtracts a number from another number"""
-        minuend = self.inputFloat("Enter Minuend number: ")
-        subtrahend = self.inputFloat("Enter Subtrahend number: ")
+        minuend = self.get_float_input("Enter Minuend number: ")
+        subtrahend = self.get_float_input("Enter Subtrahend number: ")
         print("{} - {} = {}".format(minuend, subtrahend, minuend - subtrahend))
 
     def sum(self):
         """Sums a series of numbers"""
-        count = self.inputInt("How many number do you want to sum: ")
+        count = self.get_int_input("How many number do you want to sum: ")
         sum = 0.0
         for number in range(count):
-            sum += self.inputFloat("Enter number {} of {}: ".format(number + 1, count))
+            sum += self.get_float_input(
+                "Enter number {} of {}: ".format(number + 1, count)
+            )
         print("Sum = {}".format(sum))
 
     def run(self):
         """Runs the calculator"""
         while True:
-            self.printMenu()
-            commandCode = input("Enter your choice: ")
-            match commandCode:
+            self.print_menu()
+            command_code: str = input("Enter your choice: ")
+            match command_code:
                 case "1":
                     self.sum()
                 case "2":
@@ -87,7 +116,7 @@ class CalculatorCLI:
                     print("Goodbye")
                     break
                 case _:
-                    print("Invalid Choice:", commandCode)
+                    print("Invalid Choice:", command_code)
 
 
 calculatorCLI = CalculatorCLI()
